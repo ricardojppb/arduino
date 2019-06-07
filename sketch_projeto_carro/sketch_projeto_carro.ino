88cd1009b63b1297bc34583f31e0d0cd9fba67f4
Ricardo Silvestre <ricardo.silva@receita.pb.gov.br>
2019-06-07 18:04:52 -0300

    incluindo a implementação dos ultrassonicos.

--- sketch_projeto_carro/sketch_projeto_carro.ino
+++ sketch_projeto_carro/sketch_projeto_carro.ino
@@ -6,11 +6,19 @@
 #define motor_esquerdo 4
 
 //Define os pinos para o trigger e echo
-#define pino_trigger 4
-#define pino_echo 5
+#define pUltrasonicoCentral_trigger 22
+#define pUltrasonicoCentral_echo 23
+
+#define pUltrasonicoDireita_trigger 24
+#define pUltrasonicoDireita_echo 25
+
+#define pUltrasonicoEsquerda_trigger 26
+#define pUltrasonicoEsquerda_echo 27
 
 //Inicializa o sensor nos pinos definidos acima
-Ultrasonic ultrasonic(pino_trigger, pino_echo);
+Ultrasonic ultrasonicoCentral(pUltrasonicoCentral_trigger, pUltrasonicoCentral_echo);
+Ultrasonic ultrasonicoDireita(pUltrasonicoDireita_trigger, pUltrasonicoDireita_echo);
+Ultrasonic ultrasonicoEsquerda(pUltrasonicoEsquerda_trigger, pUltrasonicoEsquerda_echo);
 
 //Definindo a classe para controlar os motores
 MotorDriver m;
@@ -25,15 +33,34 @@ void setup() {
 void loop() {
 
   //Le as informacoes do sensor, em cm e pol
-  float cmMsec, inMsec;
-  long microsec = ultrasonic.timing();
+  float cmMsecCentral, inMsecCentral, cmMsecDireita,
+        inMsecDireita, cmMsecEsquerda, inMsecEsquerda;
+
+  long microsecCentral = ultrasonicoCentral.timing();
+  long microsecDireita = ultrasonicoDireita.timing();
+  long microsecEsquerda = ultrasonicoEsquerda.timing();
 
   //Lendo em centimetros
-  cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
+  cmMsecCentral = ultrasonicoCentral.convert(microsecCentral, Ultrasonic::CM);
   //Lendo em polegadas
-  //inMsec = ultrasonic.convert(microsec, Ultrasonic::IN);
-  Serial.print("Distancia em cm: ");
-  Serial.printnl(cmMsec);
+  //inMsecCentral = ultrasonicoCentral.convert(microsecCentral, Ultrasonic::IN);
+
+  //Lendo em centimetros
+  cmMsecDireita = ultrasonicoDireita.convert(microsecDireita, Ultrasonic::CM);
+  //Lendo em polegadas
+  //inMsecDireita = ultrasonicoDireita.convert(microsecDireita, Ultrasonic::IN);
+
+  //Lendo em centimetros
+  cmMsecEsquerda = ultrasonicoEsquerda.convert(microsecEsquerda, Ultrasonic::CM);
+  //Lendo em polegadas
+  //inMsecEsquerda = ultrasonicoEsquerda.convert(microsecEsquerda, Ultrasonic::IN);
+
+  Serial.print("Distancia Central em cm: ");
+  Serial.println(cmMsecCentral);
+  Serial.print("Distancia Direita em cm: ");
+  Serial.println(cmMsecDireita);
+  Serial.print("Distancia Esquerda em cm: ");
+  Serial.println(cmMsecEsquerda);
   delay(1000);
 
 
@@ -49,6 +76,7 @@ void move_roda_direita_frente() {
 }
 
 void move_roda_esqueda_frente() {
+
   for (int i = 0; i < 255; i++) {
     // m.motor(motor_direito, FORWARD, i);
     m.motor(motor_esquerdo, FORWARD, i);
@@ -57,6 +85,7 @@ void move_roda_esqueda_frente() {
 }
 
 void move_roda_direita_traz() {
+
   for (int i = 0; i < 255; i++) {
     m.motor(motor_direito, BACKWARD, i);
     //m.motor(motor_esquerdo, BACKWARD, i);
